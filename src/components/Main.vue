5d28531ff533cb4ad712e0480cd617df46143434
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-05-13 11:30:25 -0300

    Create basic funtionality and layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,67 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+let totalIncome = ref(0)
+let totalExpenses = ref(0)
+const currentBalance = computed(() => (totalIncome.value - totalExpenses.value).toFixed(2))
+const newTransaction = ref()
+
+function addIncome() {
+    const amount = parseFloat(newTransaction.value)
+    if (isNaN(amount)) {
+        return
+    }
+    totalIncome.value += amount
+    newTransaction.value = null
+}
+
+function addExpense() {
+    const amount = parseFloat(newTransaction.value)
+    if (isNaN(amount)) {
+        return
+    }
+    totalExpenses.value += amount
+    newTransaction.value = null
+}
+</script>
+
+
 <template>
-    <div>
-        <v-card class="mx-auto" width="400" prepend-icon="mdi-currency-usd">
-            <template v-slot:title>
-                Money Tracker
-            </template>
-
-            <v-text-field label="Amount" prefix="$"></v-text-field>
-            <v-btn variant="outlined" prepend-icon="mdi-plus">Income</v-btn>
-            <v-btn variant="outlined" prepend-icon="mdi-plus">Expense</v-btn>
+    <div style="margin-top:45px; text-align: center;">
+        <v-card class="mx-auto" max-width="500" variant="outlined">
+            <div>
+                <div style="text-align: center; margin-top: 15px; font-size: x-large; font-weight:bold;">
+                    Money Tracker
+                </div>
+            </div>
+            <v-card-content>
+                <v-chip class="ma-2" close color="green" label text-color="white" variant="outlined">
+                    Balance: $ {{ currentBalance }}
+                </v-chip>
+                <v-chip class="ma-2" close color="black" label text-color="white" variant="outlined">
+                    Income: $ {{ totalIncome.toFixed(2) }}
+                </v-chip>
+                <v-chip class="ma-2" close color="red" label text-color="white" variant="outlined">
+                    Expenses: $ {{ totalExpenses.toFixed(2) }}
+                </v-chip>
+                <br />
+                <br />
+                <v-text-field v-model="newTransaction" label="Transaction Amount" variant="outlined" prefix="$" shaped>
+                </v-text-field>
+                <v-row>
+                    <v-col>
+                        <v-btn @click="addIncome"
+                            :disabled="isNaN(newTransaction) || (newTransaction == null) || (newTransaction == '')" flat
+                            prepend-icon="mdi-plus" color="black">Income</v-btn>
+                    </v-col>
+                    <v-col>
+                        <v-btn @click="addExpense"
+                            :disabled="isNaN(newTransaction) || (newTransaction == null) || (newTransaction == '')" flat
+                            prepend-icon="mdi-plus" color="red">Expense</v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-content>
         </v-card>
-
     </div>
+
 </template>
