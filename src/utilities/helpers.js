d7cfce008c519887de009802258baba6e06e3710
Dawn Wilson <16958583+dawnwilson@users.noreply.github.com>
2022-05-21 10:47:13 -0300

    Create currency conversion helper function

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,3 +1,22 @@
 export function formatCurrency(amount) {
   return `$ ${(amount / 100).toFixed(2)}`;
 }
+
+export function convertCurrencyToInteger(currency) {
+  let dollars
+  let cents
+  const periodIndex = currency.indexOf('.')
+
+  if (periodIndex === -1) {
+    dollars = currency
+    cents = '00'
+  } else {
+    dollars = currency.slice(0, periodIndex)
+    cents = currency.slice(periodIndex + 1, periodIndex + 3)
+    if (cents.length < 2) {
+      cents = `${cents}0`
+    }
+  }
+
+  return +`${dollars}${cents}`
+}
